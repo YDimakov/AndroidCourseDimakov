b97493289b9933cf6731c130fe1dd3caf3e33524
YD <YDimakov@Gmail.com>
2020-09-09 00:44:58 +0300

    Выполнил домашнюю работу №5, доработал код из ДЗ №3. Все условия выполнены в полном объеме!

--- contactsinroomlibrary/src/main/java/com/example/contactsinroomlibrary/Contact.kt
+++ contactsinroomlibrary/src/main/java/com/example/contactsinroomlibrary/Contact.kt
@@ -6,15 +6,16 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "contacts")
 class Contact(
-        @field:ColumnInfo(name = "contact_id")
-        @field:PrimaryKey(autoGenerate = true)
-        val id: Int,
+    @ColumnInfo(name = "contact_id")
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
 
-        @field:ColumnInfo(name = "contact_name")
-        var name: String,
+    @ColumnInfo(name = "contact_name")
+    var name: String,
 
-        @field:ColumnInfo(name = "contact_information")
-        var information: String,
+    @ColumnInfo(name = "contact_information")
+    var information: String,
 
-        @field:ColumnInfo(name = "contact_photo")
-        var photo: Int)
+    @field:ColumnInfo(name = "contact_photo")
+    var photo: Int
+)
